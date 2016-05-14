726940db975a906802dd5bf14a2a05e382b677b5
Justin Jones <justin@jstnjns.com>
2016-05-13 20:02:59 -0700

    Updating motion hardware code to handle motion events

--- hardware/esp8266/motion/src/main.ino
+++ hardware/esp8266/motion/src/main.ino
@@ -3,15 +3,19 @@
 #include <ArduinoJson.h>
 
 int motionPin = 0;
+bool motionState = LOW;
 
 const char* ssid = "...";
 const char* password = "...";
 
+const char* host = "localhost";
+const int port = 1337;
+const char* url = "/api/modules/9";
+
 ESP8266WebServer server(80);
 
 void setup() {
-  pinMode(powerPin, OUTPUT);
-  digitalWrite(powerPin, HIGH);
+  pinMode(motionPin, INPUT);
 
   Serial.begin(115200);
   WiFi.begin(ssid, password);
@@ -36,7 +40,61 @@ void setup() {
 }
 
 void loop() {
-  server.handleClient();
+  // server.handleClient();
+  handleMotion();
+}
+
+void handleMotion() {
+  bool motionValue = digitalRead(motionPin);
+
+  bool highToLow = motionValue == HIGH && motionState == LOW;
+  bool lowToHigh = motionValue == LOW && motionState == HIGH;
+
+  if (highToLow || lowToHigh) {
+    motionState = motionValue;
+    request();
+  }
+
+  delay(100);
+}
+
+void request() {
+  WiFiClient client;
+
+  Serial.println("Making request...");
+
+  if (!client.connect(host, port)) {
+    Serial.println("Cannot connect...");
+    return;
+  }
+
+  client.print("PUT ");
+  client.print(url);
+  client.println(" HTTP/1.1");
+
+  client.print("Host: ");
+  client.println(host);
+
+  client.println("Connection: close");
+
+  client.println();
+
+  client.print("values[motion]=");
+  client.println(digitalRead(motionPin)?"true":"false");
+
+  while(client.available()){
+    String line = client.readStringUntil('\r');
+    Serial.print(line);
+  }
+
+  unsigned long timeout = millis();
+  while (client.available() == 0) {
+    if (millis() - timeout > 5000) {
+      Serial.println("Timeout...");
+      client.stop();
+      return;
+    }
+  }
 }
 
 void respond() {
