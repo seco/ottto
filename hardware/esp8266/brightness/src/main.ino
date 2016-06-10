3226d2f3f4cd559d3c5b6b1ed7eafeb559e53fee
Justin Jones <justin@jstnjns.com>
2016-06-09 21:51:50 -0700

    Adding brightness

--- hardware/esp8266/brightness/src/main.ino
+++ hardware/esp8266/brightness/src/main.ino
@@ -1,17 +1,21 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
+#include <ESP8266HTTPClient.h>
 #include <ArduinoJson.h>
 
-int powerPin = 0;
+const uint8_t photoPin = 0;
 
 const char* ssid = "...";
 const char* password = "...";
 
+const char* host = "localhost";
+const int port = 1337;
+const char* url = "/api/modules/...";
+
 ESP8266WebServer server(80);
 
 void setup() {
-  pinMode(powerPin, OUTPUT);
-  digitalWrite(powerPin, HIGH);
+  pinMode(photoPin, INPUT);
 
   Serial.begin(115200);
   WiFi.begin(ssid, password);
@@ -21,8 +25,7 @@ void setup() {
     Serial.print(".");
   }
 
-  server.on("/", request);
-  // server.onNotFound(request);
+  server.on("/", respond);
   server.begin();
 
   Serial.println("");
@@ -37,35 +40,51 @@ void setup() {
 }
 
 void loop() {
-  server.handleClient();
+  // server.handleClient();
+  handleLight();
+}
+
+void handleLight() {
+  request();
+  delay(1000);
 }
 
 void request() {
-  (server.method() == HTTP_GET) ? get() : post();
-}
+  HTTPClient http;
 
-void get() {
-  respond();
+  String body = "values[level]=";
+  body += getBrightness();
+
+  http.begin("http://10.0.0.6:1337/api/modules/20");
+  http.addHeader("Content-Type", "application/x-www-form-urlencoded");
+  http.POST(body);
+  http.writeToStream(&Serial);
+  http.end();
 }
 
 void respond() {
+  get();
+}
+
+void get() {
+  output();
+}
+
+void output() {
   StaticJsonBuffer<200> jsonBuffer;
   JsonObject& json = jsonBuffer.createObject();
 
-  bool powerValue = digitalRead(powerPin);
-  json["power"] = powerValue;
+  json["level"] = getBrightness();
 
   String body;
   json.prettyPrintTo(body);
+  json.prettyPrintTo(Serial);
   server.send(200, "application/json; charset=utf-8", body);
 }
 
-void post() {
-  receive();
-  respond();
-}
 
-void receive() {
-  bool powerValue = server.arg("power") == "true";
-  digitalWrite(powerPin, powerValue);
+int getBrightness() {
+  int brightness = map(analogRead(photoPin), 0, 1024, 0, 4);
+  Serial.println(brightness);
+  return brightness;
 }
