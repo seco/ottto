8bdc66b4221238d45452e7d2d8a6d169bd7317d3
Justin Jones <justin@jstnjns.com>
2017-05-23 00:30:52 -0700

    Boolean cleanup

--- native/app/components/modules/attributes/boolean.js
+++ native/app/components/modules/attributes/boolean.js
@@ -3,23 +3,31 @@ import { StyleSheet, View, Text, Switch } from 'react-native'
 
 
 class Boolean extends Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      value: props.value
+    }
+  }
+
   render() {
-    const { attribute, value } = this.props
+    const { attribute } = this.props
+
     const truthy = attribute.options[0]
     const falsey = attribute.options[1]
 
     return (
       <View>
-        <Switch value={value}
+        <Switch value={this.state.value}
           onValueChange={this.onValueChange.bind(this)} />
       </View>
     )
   }
 
   onValueChange(value) {
-    this.setState({ value })
-
-    this.props.onValueChange && this.props.onValueChange(value)
+    this.setState({ value: value })
+    this.props.onValueChange(value)
   }
 }
 
