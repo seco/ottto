afa747f0d9b31c28633fbf0545af53dbfaa06f35
Justin Jones <justin@jstnjns.com>
2017-06-02 20:42:05 -0700

    Renaming motion-mqtt to motion

--- modules/motion/src/main.ino
+++ modules/motion/src/main.ino
@@ -1,112 +1,141 @@
-#include <DNSServer.h>
 #include <ESP8266WiFi.h>
-#include <ESP8266WebServer.h>
-#include <ESP8266HTTPClient.h>
+#include <PubSubClient.h>
 #include <WiFiManager.h>
 #include <ArduinoJson.h>
 
 
-const int motionPin = D2;
+const int motionPin = D0;
 bool motionState = LOW;
-String baseAddress = "http://192.168.1.11:1337";
 
-ESP8266WebServer server(80);
+const char* serverIp = "192.168.1.9";
+const int mqttPort = 1883;
+const int httpPort = 1337;
+
+const char* moduleName = "motionsensor";
+
 WiFiManager wifiManager;
+WiFiClient espClient;
+PubSubClient client(espClient);
+
 
 
 void setup() {
   pinMode(motionPin, INPUT);
 
   Serial.begin(115200);
-  wifiManager.autoConnect("Motion Sensor");
 
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
-  Serial.print("Mac address: ");
-  Serial.println(WiFi.macAddress());
-  Serial.print("Chip: ");
-  Serial.println(ESP.getChipId());
-
-  connect();
-  send();
-
-  server.on("/", respond);
-  server.begin();
+  setupWifi();
+  setupMqttClient();
 }
 
 
-void connect() {
-  StaticJsonBuffer<200> jsonBuffer;
-  JsonObject& json = jsonBuffer.createObject();
-  json["chip"] = ESP.getChipId();
-  json["ip"] = WiFi.localIP().toString();
-
-  String body;
-  json.prettyPrintTo(body);
-  Serial.println(body);
-
-  HTTPClient http;
-  http.begin(baseAddress + "/api/modules/register");
-  http.addHeader("Content-Type", "application/json");
-  http.POST(body);
-  http.end();
+void setupWifi() {
+  wifiManager.autoConnect(moduleName);
 }
+
+
+void setupMqttClient() {
+  client.setServer(serverIp, mqttPort);
+  client.setCallback(callback);
+}
+
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.print("Received: ");
+  Serial.print(topic);
+  Serial.print(": ");
+
+  char message[length + 1];
+  for (int i = 0; i < length; i++) {
+    message[i] = (char)payload[i];
+  }
+  message[length] = '\0';
+  Serial.println(message);
+
+  // processJson(message);
+}
+
+// bool processJson(char* message) {
+//   StaticJsonBuffer<200> jsonBuffer;
+//   JsonObject& json = jsonBuffer.parseObject(message);
+//
+//   if (!json.success()) {
+//     Serial.println("parseObject() failed");
+//   }
+//
+//   // Do something with the new data
+// }
 
 
 void loop() {
-  server.handleClient();
-  handleMotion();
+  if (!client.connected()) {
+    connectToServer();
+  }
 
-  delay(100);
-}
+  client.loop();
 
-
-void handleMotion() {
   bool motionValue = digitalRead(motionPin);
-
   bool highToLow = motionValue == HIGH && motionState == LOW;
   bool lowToHigh = motionValue == LOW && motionState == HIGH;
 
   if (highToLow || lowToHigh) {
     motionState = motionValue;
-    send();
+    sendState();
   }
 }
 
 
-void send() {
+void connectToServer() {
+  while (!client.connected()) {
+    Serial.print("Attempting MQTT connection...");
+    if (client.connect(moduleName)) {
+      Serial.println("connected");
+      registerWithServer();
+      client.subscribe("modules/9");
+      sendState();
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
+
+
+void registerWithServer() {
+  StaticJsonBuffer<200> jsonBuffer;
+  JsonObject& json = jsonBuffer.createObject();
+  json["chip"] = ESP.getChipId();
+  json["ip"] = WiFi.localIP().toString();
+  json["type"] = "motion";
+
+  char buffer[json.measureLength() + 1];
+  Serial.print("modules/register: ");
+  json.printTo(buffer, sizeof(buffer));
+  Serial.println(buffer);
+
+  client.publish("modules/register", buffer);
+}
+
+
+void sendState() {
   StaticJsonBuffer<200> jsonBuffer;
   JsonObject& json = jsonBuffer.createObject();
   JsonObject& values = json.createNestedObject("values");
   values["motion"] = getMotion();
 
-  String body;
-  json.prettyPrintTo(body);
-  Serial.println(body); // Debugging
+  char buffer[json.measureLength() + 1];
+  json.printTo(buffer, sizeof(buffer));
+  Serial.print("Sending: ");
+  Serial.print("modules/9: ");
+  Serial.println(buffer);
 
-  HTTPClient http;
-  http.begin(baseAddress + "/api/modules/9");
-  http.addHeader("Content-Type", "application/json");
-  http.POST(body);
-  http.end();
+  client.publish("modules/9", buffer, true);
 }
 
 
-void respond() {
-  StaticJsonBuffer<200> jsonBuffer;
-  JsonObject& json = jsonBuffer.createObject();
-  JsonObject& values = json.createNestedObject("values");
-  values["motion"] = getMotion();
-
-  String body;
-  json.prettyPrintTo(body);
-  Serial.println(body); // Debugging
-
-  server.send(200, "application/json; charset=utf-8", body);
-}
-
-
-String getMotion() {
-  String motionValue = digitalRead(motionPin) ? "true" : "false";
+bool getMotion() {
+  bool motionValue = digitalRead(motionPin) ? true : false;
   return motionValue;
 }
